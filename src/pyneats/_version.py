a6a5739657b7202de6237fa5efeb0919280e33bb
JarryGabriel <jarry.gabriel@gmail.com>
2026-02-16 11:54:13 +0100

    chore: update dev version
    Auto-generated by setuptools-scm.
    Tracked for now; see #45 to gitignore it.

--- src/pyneats/_version.py
+++ src/pyneats/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.7.1.dev9+g75c93908f'
-__version_tuple__ = version_tuple = (0, 7, 1, 'dev9', 'g75c93908f')
+__version__ = version = '0.7.4.dev0+g39c602a3c.d20260216'
+__version_tuple__ = version_tuple = (0, 7, 4, 'dev0', 'g39c602a3c.d20260216')
 
 __commit_id__ = commit_id = None
